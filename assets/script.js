40b722e39d363c7a4e41f513531bfd795d34ca79
Moonbeam <mfzernick@gmail.com>
2022-12-29 13:10:49 -0600

    no more undefined errors

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,14 @@ var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","
 var numbers = ["1","2","3","4","5","6","7","8","9"];
 var specialCharacters = ["!",".","#","$","%","&","*"];
 
+function createPassword() {
 var passLength =window.prompt("Would you like to include numbers?");
 if (passLength == "yes"){
     var newPassword1 = numbers[Math.floor(Math.random()*numbers.length)] +numbers[Math.floor(Math.random()*numbers.length)]+numbers[Math.floor(Math.random()*numbers.length)];
     alert ("Your password will contain " + newPassword1);
+} else{
+    var newPassword2 = specialCharacters[Math.floor(Math.random()*specialCharacters.length)] +specialCharacters[Math.floor(Math.random()*specialCharacters.length)]+specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
+    var newPassword1 =("");
 };
 var passLetters = window.prompt("Would you like to include letters?");
 if (passLetters == "yes"){
@@ -17,6 +21,7 @@ if (passLetters == "yes"){
 else {
     var newPassword1 = numbers[Math.floor(Math.random()*numbers.length)] +numbers[Math.floor(Math.random()*numbers.length)]+numbers[Math.floor(Math.random()*numbers.length)]+numbers[Math.floor(Math.random()*numbers.length)];
     alert ("Your password will contain " + newPassword1);
+    var newPassword =("");
 };
 var passSpecial =window.prompt("Would you like to include special characters?")
 if (passSpecial == "yes"){
@@ -25,10 +30,11 @@ if (passSpecial == "yes"){
 }else {
     var newPassword = letters[Math.floor(Math.random()*letters.length)] +letters[Math.floor(Math.random()*letters.length)]+letters[Math.floor(Math.random()*letters.length)]+letters[Math.floor(Math.random()*letters.length)];
     alert ("Your password will contain " + newPassword + newPassword1); 
+    var newPassword2 =("");
 };
 
-function createPassword() {
+
  alert("Your new password is: " +newPassword+newPassword1+newPassword2 );
 
-}
+};
 createPassword();
